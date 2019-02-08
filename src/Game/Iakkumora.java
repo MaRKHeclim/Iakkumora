f19a36a592dbbe6c5a8465d2443939db899403fe
herptoderp <mark5000432@hotmail.com>
2019-02-08 03:34:26 -0600

    Modifications to get a window with a button.

--- src/Game/Iakkumora.java
+++ src/Game/Iakkumora.java
@@ -2,6 +2,11 @@ package Game;
 
 import Tools.Debug;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+import javax.swing.*;
+
 public class Iakkumora extends javax.swing.JFrame
 {
 	Debug debug = new Debug("Iakkumora", true, false);
@@ -14,6 +19,7 @@ public class Iakkumora extends javax.swing.JFrame
 		
 	}
 	
+	Iakkumora game;
 	Iakkumora() // constructor
 	{
 		
@@ -22,6 +28,21 @@ public class Iakkumora extends javax.swing.JFrame
 	
 	void startGame() //main method
 	{
+		game = new Iakkumora();
+		game.setLayout(new FlowLayout());
+		game.setVisible(true);
+		game.setSize(650, 560);
+		game.setLocationRelativeTo(null);
+		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.setTitle("Iakkumora");
+		game.setBackground(new Color(160, 30, 120));
 		
+		JButton newGameButton = new JButton("New Game");
+		newGameButton.setSize(200,100);
+		newGameButton.setBackground(new Color(30,190,190));
+		newGameButton.setMnemonic(KeyEvent.VK_N);
+		
+		
+		game.add(newGameButton);
 	}
 }
