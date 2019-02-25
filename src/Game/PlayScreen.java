6fccc98f0a7380117fd5c538d69c5cb37029814e
herptoderp <mark5000432@hotmail.com>
2019-02-25 00:29:57 -0600

    added some flesh to the PlayScreen (mostly comments, but it's still something)

--- src/Game/PlayScreen.java
+++ src/Game/PlayScreen.java
@@ -1,12 +1,14 @@
 package Game;
 
+import Tools.Debug;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class PlayScreen extends JPanel implements ActionListener
 {
-	
+	Debug debug = new Debug("PlayScreen", true, false);
 	
 	PlayScreen() //constructor
 	{
@@ -29,22 +31,43 @@ public class PlayScreen extends JPanel implements ActionListener
 		super.paint(g);
 		Graphics2D g2d = (Graphics2D) g;
 		//TODO set font if needed
+		//TODO Objects should know how to draw themselves!!!
+		//TODO map.draw(); or map.draw(x, y);
 		
 	}
 	
 	//TODO Keyboard Events
+	//Adapter over listener so that not all methods have to be defined
+	//there is a keyTyped event that may be useful if non-keyboard characters may be entered
+	//		for example if an alt-code is used - it may??? provide the character
+	// 			≈ instead of ALT, 0, 2, 4, 7...
 	private class KL extends KeyAdapter
 	{
 		@Override
-		public void keyReleased(KeyEvent e)
+		public void keyPressed(KeyEvent e)
 		{
-		
+			int keyCode = e.getKeyCode();
+			switch (keyCode) {
+				case KeyEvent.VK_ESCAPE:
+					//TODO add a prompt hefore exiting
+					System.exit(0);
+				break;
+				
+				default:
+				
+				break;
+			}
 		}
 		
 		@Override
-		public void keyPressed(KeyEvent e)
+		public void keyReleased(KeyEvent e)
 		{
-		
+			int keyCode = e.getKeyCode();
+			switch (keyCode) {
+				default:
+				
+				break;
+			}
 		}
 	}
 	
@@ -61,6 +84,16 @@ public class PlayScreen extends JPanel implements ActionListener
 		public void mouseReleased(MouseEvent e)
 		{
 		
+		}
+	}
+	
+	//TODO Window Events
+	private class WL extends WindowAdapter
+	{
+		@Override
+		public void windowClosing(WindowEvent e)
+		{
+			//try {Thread.currentThread().join();} catch (interruptedException ex) {}
 		}
 	}
 }
