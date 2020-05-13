1349a0911a2136633614c235faf021aaca9410a2
Matthew <mark5000432@hotmail.com>
2020-05-12 23:07:00 -0500

    Update Tile.java
    Added Constructor for loading a pregenerated tile.

--- src/Map/Tile.java
+++ src/Map/Tile.java
@@ -10,6 +10,12 @@ public class Tile
 	int x, y;
 	int biome;
 	
+	/**
+	 * Constructor - Used to Generate a New Tile
+	 * 
+	 * @param x The x value of the tile to be generated within the chunk.
+	 * @param y The y value of the tile to be generated within the chunk.
+	 */
 	Tile(int x, int y)//constructor
 	{
 		this.x = x;
@@ -17,6 +23,21 @@ public class Tile
 		biome = fastfloor(scale(Map.mapRNG.next(), 0, 65538, 0, 6));
 	}
 	
+	/**
+	 * Constructor - Used to Load a Pregenerated Tile
+	 * 
+	 * @param x 		The x value of the tile within the chunk.
+	 * @param y 		The y value of the tile within the chunk.
+	 * @param biome		The biome of the tile. 
+	 */
+	Tile(int x, int y, int biome)
+	{
+		this.x = x;
+		this.y = y;
+		this.biome = biome;
+	}
+	
+	//TODO add Description here
 	void draw(Graphics2D g2d, int horizontalOffset, int verticalOffset, int scale)
 	{
 		g2d.setColor(Map.getColor(biome));
